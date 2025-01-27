2c98d83c45ccb9ffa269076a58049e8e139c3d27
ricarjust <ricartoonstudio@gmail.com>
2025-01-27 07:38:11 -0300

    feat: add gitignore :/

--- StudentService/obj/Debug/net9.0/StudentService.AssemblyInfo.cs
+++ StudentService/obj/Debug/net9.0/StudentService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7bffa8e30b94766dbe7f0f8991b3f56c138210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575c532ef073198000b347b993c787d3a1485b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
